5a77a0573abfcd59cd4e74388ae4abe1a7e8441c
Abhinav20007 <79750085+Abhinav20007@users.noreply.github.com>
2021-05-28 16:30:04 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,33 @@
+const accessCode1 = "CONSOLELOG";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("orange")
+    textSize(20)
+    text("O O O L L G N C S E", 100,120)
+    fill("lightblue")
+    text("Hint: Helpful For Coders!!", 100,100)
+// add code for displaying rest of the names and the hints.
+fill("white")
+textSize(20)
+text(" N F C I N U O T", 792,200)
+fill("lightblue")
+text("Hint: Performs A Particular Task !!", 792,180)
+
+fill("green")
+textSize(20)
+text("A T E D A S B A", 90,440)
+fill("lightblue")
+text("Hint: Stores all information !!", 90,420)
+
+textSize(25)
+fill("Yellow")
+
+text("*NOTE : DO NOT USE SPACE KEY !!", 692,90)
+fill("red")
+text("*NOTE : DO NOT USE SPACE KEY !!", 694,90)
+fill("black")
+text("*NOTE : DO NOT USE SPACE KEY !!", 690,90)
+}
